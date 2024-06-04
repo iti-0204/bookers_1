a4ec9a99702af4aaaa5cbc99e2b1c3083d5abcb8
iti-0204 <84443576+iti-0204@users.noreply.github.com@users.noreply.github.com>
2024-06-04 06:08:49 +0000

    [Add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,18 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to "/books"
+    if book.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to book_path(book.id)
+    else
+      @book = Book.new
+      @books = Book.all
+      flash.now[:notice] = "投稿に失敗しました。"
+      render :index
+    end
+
+
+
   end
 
   def show
@@ -23,8 +33,14 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      flash.now[:notice] = "投稿に失敗しました。"
+      render :edit
+    end
   end
 
   def destroy
